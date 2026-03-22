dc06d8758b8aa1e3a3198833630fccdddad80663
erik-ng-3006 <erikng3006@gmail.com>
2026-03-22 22:49:25 +0700

    Implement api routes for create and read event

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,27 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/ingest/static/:path*',
-        destination: 'https://us-assets.i.posthog.com/static/:path*',
-      },
-      {
-        source: '/ingest/:path*',
-        destination: 'https://us.i.posthog.com/:path*',
-      },
-    ];
-  },
-  skipTrailingSlashRedirect: true,
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'res.cloudinary.com',
+			},
+		],
+	},
+	async rewrites() {
+		return [
+			{
+				source: '/ingest/static/:path*',
+				destination: 'https://us-assets.i.posthog.com/static/:path*',
+			},
+			{
+				source: '/ingest/:path*',
+				destination: 'https://us.i.posthog.com/:path*',
+			},
+		];
+	},
+	skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
